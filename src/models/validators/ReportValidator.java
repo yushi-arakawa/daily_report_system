7901ac83de971febd178728d98056778cb967a76
yushi <stargazer-4991@outlook.jp>
2020-10-25 17:59:23 +0900

    Add Report create

--- src/models/validators/ReportValidator.java
+++ src/models/validators/ReportValidator.java
@@ -6,36 +6,35 @@ import java.util.List;
 import models.Report;
 
 public class ReportValidator {
-    private static List<String> vaildate(Report r) {
+    public static List<String> validate(Report r) {
         List<String> errors = new ArrayList<String>();
 
-        String title_error = _vaildateTitle(r.getTitle());
+        String title_error = _validateTitle(r.getTitle());
         if(!title_error.equals("")) {
-            errors.add("title_error");
+            errors.add(title_error);
         }
 
-        String content_error = _vaildateContent(r.getContent());
+        String content_error = _validateContent(r.getContent());
         if(!content_error.equals("")) {
-            errors.add("content_error");
+            errors.add(content_error);
         }
 
         return errors;
     }
 
-    private static String _vaildateTitle(String title) {
+    private static String _validateTitle(String title) {
         if(title == null || title.equals("")) {
             return "タイトルを入力してください。";
-        }
+            }
 
         return "";
     }
 
-    private static String _vaildateContent(String content) {
+    private static String _validateContent(String content) {
         if(content == null || content.equals("")) {
             return "内容を入力してください。";
-        }
+            }
 
         return "";
     }
-
 }
